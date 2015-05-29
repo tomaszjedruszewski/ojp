c5efb05b4f75b2f362e80aa86f11976b5494ec45
tomaszjedruszewski <tomasz.jedruszewski@gmail.com>
2015-05-29 20:09:41 +0200

    Przekazywanie danych między formularzami
    Doprowadzenie do przekazywania danych między dwoma formularzami - główne
    założenie projektu.

--- WindowsFormApplication1/Form1.h
+++ WindowsFormApplication1/Form1.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Form2.h"
 
 namespace WindowsFormApplication1 {
 
@@ -34,6 +35,16 @@ namespace WindowsFormApplication1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	protected:
+
+	protected:
+
+	protected:
+
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+
 
 	private:
 		/// <summary>
@@ -48,13 +59,76 @@ namespace WindowsFormApplication1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(6) { L"5", L"10", L"20", L"50", L"100", L"200" });
+			this->comboBox1->Location = System::Drawing::Point(78, 67);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(121, 21);
+			this->comboBox1->TabIndex = 1;
+			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &Form1::comboBox1_SelectedIndexChanged);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(78, 144);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(121, 23);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Szukaj";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(12, 94);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(260, 20);
+			this->textBox1->TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 262);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->comboBox1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		
+		Form2^ form2 = gcnew Form2;
+		form2->label1->Text = textBox1->Text;
+		
+		form2->ShowDialog();
+		
+	}
+	private: System::Void comboBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		if (comboBox1->Text == "5")
+			textBox1->Text = "Wybra³eœ do³adowanie za 5 z³";
+		if (comboBox1->Text == "10")
+			textBox1->Text = "Wybra³eœ do³adowanie za 10 z³";
+		if (comboBox1->Text == "20")
+			textBox1->Text = "Wybra³eœ do³adowanie za 20 z³";
+		if (comboBox1->Text == "50")
+			textBox1->Text = "Wybra³eœ do³adowanie za 50 z³";
+		if (comboBox1->Text == "100")
+			textBox1->Text = "Wybra³eœ do³adowanie za 100 z³";
+		if (comboBox1->Text == "200")
+			textBox1->Text = "Wybra³eœ do³adowanie za 200 z³";
+	}
+};
 }
 
---
+++ WindowsFormApplication1/Form2.h
@@ -0,0 +1,102 @@
+#pragma once
+
+namespace WindowsFormApplication1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form2
+	/// </summary>
+	public ref class Form2 : public System::Windows::Forms::Form
+	{
+	public:
+		Form2(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form2()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	public: System::Windows::Forms::Label^  label1;
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(86, 101);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"label1";
+			// 
+			// Form2
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 262);
+			this->Controls->Add(this->label1);
+			this->Name = L"Form2";
+			this->Text = L"Form2";
+			this->Load += gcnew System::EventHandler(this, &Form2::Form2_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void Form2_Load(System::Object^  sender, System::EventArgs^  e) {
+	}
+	};
+}
